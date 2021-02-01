46608ffef19608f5f30a85316193cabda4d6560e
Dom <59603300+TokyoDom@users.noreply.github.com>
2021-02-02 02:44:15 +0900

    POST leaders endpoint

--- routes.js
+++ routes.js
@@ -4,22 +4,43 @@ const router = express.Router();
 const knexConfig = require("./knexfile");
 const knex = require('knex')(knexConfig);
 
-router.get("/leaders", async (req, res) => {
-  let data = await knex.select().from("gym_leaders");
+function updatePokeArr(data) {
+  return data.map(leader => {
+    if (leader.pokemon) leader.pokemon = leader.pokemon.split(",");
+    return leader;
+  });
+}
 
-  let limit = req.query.limit;
-  if (typeof limit !== "undefined") {
-    limit = parseInt(limit);
-
-    if(Number.isNaN(limit)) {
+router.route("/leaders")
+  .get(async (req, res) => {
+    let data = await knex("gym_leaders");
+    data = updatePokeArr(data);
+  
+    let limit = req.query.limit;
+    if (typeof limit !== "undefined") {
+      limit = parseInt(limit);
+  
+      if(Number.isNaN(limit)) {
+        res.sendStatus(400);
+        return;
+      } else {
+        data = data.slice(0, limit);
+      }
+    }
+    
+    res.json(data);
+  })
+  .post(async (req, res) => {
+    try {
+      await knex("gym-leaders").insert(req.body);
+    } catch (err) {
       res.sendStatus(400);
       return;
-    } else {
-      data = data.slice(0, limit);
     }
-  }
-  
-  res.json(data);
-});
+    res.status(201).send("Success");
+  })
+  .patch(async (req, res) => {
+
+  });
 
 module.exports = router;
